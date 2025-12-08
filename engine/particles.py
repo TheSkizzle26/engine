b1f4e646439eed00cf6348b26503a503b6e69ac6
TheSkizzle26 <timobarth12@outlook.de>
2025-12-08 16:56:24 +0100

    finished (for now) particle system

--- engine/particles.py
+++ engine/particles.py
@@ -4,7 +4,8 @@ import engine
 class Particle(engine.Entity):
     def __init__(self, texture, pos,
                  velocity=(0, 0), drag=0.9,
-                 gravity=(0, 10), bounce_mult=0.7):
+                 gravity=(0, 20), bounce_mult=0.7,
+                 lifetime=2, fade=True, fade_expo=1):
         super().__init__(pos, (0, 0))
 
         self.texture = texture
@@ -13,32 +14,62 @@ class Particle(engine.Entity):
         self.gravity = gravity
         self.bounce_mult = bounce_mult
 
+        self.lifetime = lifetime
+        self.spawn_time = engine.get_time()
+
+        self.fade = fade
+        self.fade_expo = fade_expo
+
         self.collision_rects = []
+
+        self.manager = None
+
+    def set_manager(self, manager):
+        self.manager = manager
 
     def set_collision_rects(self, collision_rects):
         self.collision_rects = collision_rects
 
+    def is_dead(self):
+        now = engine.get_time()
+        return now - self.spawn_time > self.lifetime
+
     def update(self):
         delta = engine.get_frame_time()
 
-        self.velocity[0] *= delta * self.drag
-        self.velocity[1] *= delta * self.drag
+        self.velocity[0] *= (1 - self.drag * delta)
+        self.velocity[1] *= (1 - self.drag * delta)
 
-        self.velocity[0] += self.gravity[0]
-        self.velocity[1] += self.gravity[1]
+        self.velocity[0] += self.gravity[0] * delta
+        self.velocity[1] += self.gravity[1] * delta
 
-        self.move_and_collide(self.collision_rects, self.velocity)
+        frame_movement = (
+            self.velocity[0] * delta,
+            self.velocity[1] * delta,
+        )
+        self.move_and_collide(self.collision_rects, frame_movement)
 
         if self.collisions["left"] or self.collisions["right"]:
-            self.velocity[0] *= -1
+            self.velocity[0] *= -self.bounce_mult
         if self.collisions["up"] or self.collisions["down"]:
-            self.velocity[1] *= -1
+            self.velocity[1] *= -self.bounce_mult
 
     def render(self):
+        time = engine.get_time()
+        t = (time - self.spawn_time) / self.lifetime
+        t **= self.fade_expo
+        a = min(max(1 - t, 0), 1)
+
         x = self.pos[0] - self.texture.width/2
         y = self.pos[1] - self.texture.height/2
+        color = (
+            255,
+            255,
+            255,
+            int(a*255)
+        )
 
-        engine.draw_texture(self.texture, int(x), int(y), engine.WHITE)
+        engine.draw_texture(self.texture, int(x), int(y), color)
 
 
 class ParticleManager(engine.ElementSingleton):
@@ -46,14 +77,24 @@ class ParticleManager(engine.ElementSingleton):
     Not sure how fast this is.
     """
 
-    def __init__(self, chunk_size):
+    def __init__(self, chunk_size=32):
         super().__init__()
 
         self.chunks = {}
         self.chunk_size = chunk_size
 
+        self.num_particles = 0
+
     def chunk_id(self, chunk_pos):
         return f"{int(chunk_pos[0])};{int(chunk_pos[1])}"
+
+    def del_particle(self, particle: Particle):
+        chunk_pos = (
+            particle.pos[0] // self.chunk_size,
+            particle.pos[1] // self.chunk_size
+        )
+
+        self.chunks[self.chunk_id(chunk_pos)].remove(particle)
 
     def add_particle(self, particle: Particle):
         chunk_pos = (
@@ -62,18 +103,17 @@ class ParticleManager(engine.ElementSingleton):
         )
 
         self.chunks.setdefault(self.chunk_id(chunk_pos), []).append(particle)
-
-    def update_particle_chunk_pos(self, particle):
-        chunk_pos = (
-            particle.pos[0] // self.chunk_size,
-            particle.pos[1] // self.chunk_size
-        )
+        particle.set_manager(self)
 
     def update(self):
         new_chunks = {}
+        num = 0
 
         for chunk in self.chunks.values():
             for particle in chunk:
+                if particle.is_dead():
+                    continue # delete it from new dict
+
                 particle.update()
 
                 chunk_pos = (
@@ -82,6 +122,7 @@ class ParticleManager(engine.ElementSingleton):
                 )
 
                 new_chunks.setdefault(self.chunk_id(chunk_pos), []).append(particle)
+                num += 1
 
         self.chunks = new_chunks
 
@@ -97,7 +138,7 @@ class ParticleManager(engine.ElementSingleton):
         )
 
         for cy in range(int(topleft_chunk_pos[1] - 1),
-                        int(topleft_chunk_pos[1] + camera_size[1]//self.chunk_size + 4)):
+                        int(topleft_chunk_pos[1] + camera_size[1]//self.chunk_size + 2)):
             for cx in range(int(topleft_chunk_pos[0] - 1),
                             int(topleft_chunk_pos[0] + camera_size[0]//self.chunk_size + 2)):
                 chunk_id = self.chunk_id((cx, cy))
---
+++ particle_test.py
@@ -0,0 +1,65 @@
+import random
+
+import engine
+from engine import Particle
+
+
+class Main(engine.Program):
+    def __init__(self):
+        super().__init__()
+        engine.init(
+            (800, 600),
+            "particle test",
+            image_path="assets/images",
+            input_map_path="assets/input_map.json"
+        )
+
+        self.camera = engine.Camera((0, 0), 4, 1)
+
+        self.particle_texture = engine.assets.load_texture("particle.png")
+        self.particles = engine.ParticleManager()
+
+        self.last_particle_time = 0
+
+    def update(self):
+        if engine.input.is_pressed("quit"):
+            self.quit()
+
+        if engine.input.is_down("log") and engine.get_time() - self.last_particle_time >= 0.015:
+            self.last_particle_time = engine.get_time()
+
+            m_pos = (
+                (engine.get_mouse_x() - 400) / 4,
+                (engine.get_mouse_y() - 300) / 4
+            )
+
+            offset = 250
+
+            self.particles.add_particle(Particle(
+                self.particle_texture,
+                m_pos,
+                velocity=(
+                    random.random() * offset - offset/2,
+                    random.random() * -offset/2,
+                ),
+                gravity=(0, 100),
+                fade_expo=3
+            ))
+
+        self.particles.update()
+
+    def render(self):
+        engine.begin_drawing()
+        engine.clear_background(engine.BLACK)
+
+        self.camera.begin()
+        self.particles.render()
+        self.camera.end()
+
+        engine.draw_text(f"FPS: {engine.get_fps()}", 2, 0, 60, engine.WHITE)
+
+        engine.end_drawing()
+
+
+if __name__ == "__main__":
+    Main().run()
